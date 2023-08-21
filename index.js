b88b13c54c36c3fa0d3f8166d93c1019f7c60cc1
Taump <taump@yandex.ru>
2023-08-21 23:28:53 +0500

    fix

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const Koa = require('koa');
 const Router = require('@koa/router');
+const svg2png = require("svg2png");
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/img/:id', (ctx, next) => {
+router.get('/img/:id', async (ctx, next) => {
 
 	const id = ctx.request.params.id;
 
-	ctx.body = `<svg width="300" height="300">
+	const img = `<svg width="300" height="300">
 		<rect x="0" y="0" width="300" height="300" stroke="red" stroke-width="3px" fill="white"/>
 		<text x="50%" y="50%" dominant-baseline="middle" style="font-size: 48px;" text-anchor="middle">
 			LINE - ${id}
 		</text>    
   	</svg>`;
 
-	ctx.type = "image/svg+xml";
+	ctx.body = await svg2png(img, { width: 300, height: 300 });
+
+
+	ctx.type = "image/png";
 })
 
 app
-  .use(router.routes())
-  .use(router.allowedMethods());
+	.use(router.routes())
+	.use(router.allowedMethods());
 
 app.listen(3030);
 
